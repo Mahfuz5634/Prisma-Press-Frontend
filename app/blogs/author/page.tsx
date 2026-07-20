ddc4103ebc00dbb87ca5135f18cd50d1df67d0e1
Mahfuz <mahfuzru5634@gmail.com>
2026-07-20 22:08:13 +0600

    Add Author page and Blogs layout; enhance Home page with Blogs link

---
+++ app/blogs/author/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import page from '../page'
+
+const AuthorPage = () => {
+  return (
+    <div>author page</div>
+  )
+}
+
+export default AuthorPage 
--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
+      <h1><a href="/blogs">blogs</a></h1>
     </div>
   );
 }
